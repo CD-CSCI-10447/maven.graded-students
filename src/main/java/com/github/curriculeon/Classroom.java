b936d660b1a66c48206cdcb69972b97f7d4e997b
mhayesCS <97618624+mhayesCS@users.noreply.github.com>
2023-03-27 15:00:04 -0400

    Almost Done Classroom Class :)

--- src/main/java/com/github/curriculeon/Classroom.java
+++ src/main/java/com/github/curriculeon/Classroom.java
@@ -1,31 +1,43 @@
 package com.github.curriculeon;
 
 
-import java.util.Map;
+import java.util.*;
 
 public class Classroom {
 
+    private List<Student> studentList;
+
     public Classroom(Student[] students) {
+        this.studentList = new ArrayList<>(Arrays.asList(students));
     }
 
     public Classroom() {
+        studentList = new ArrayList<>(Collections.emptyList());
     }
 
     public Student[] getStudents() {
-        return null;
+        return studentList.toArray(new Student[0]);
     }
 
 
     public Double getAverageExamScore() {
-        return null;
+        double ans = 0;
+        int tracker = 0;
+        for(Student i : studentList){
+            ans += i.getSumOfExamScore();
+            tracker += i.getExamScores().length;
+        }
+        return ans / tracker;
     }
 
     public Boolean addStudent(Student students) {
-        return null;
+        studentList.add(students);
+        return studentList.contains(students);
     }
 
     public Boolean removeStudent(Student student) {
-        return null;
+        studentList.remove(student);
+        return !studentList.contains(student);
     }
 
     public Student[] getStudentsByScore() {
